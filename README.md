251d8701fedfee5d541985bdf9122c058b531477
AnatomicJC <jcvassort@mandriva.com>
2014-01-02 20:49:12 +0100

    Update README.md and add python script

--- README.md
+++ README.md
@@ -2,3 +2,54 @@ mailchecker
 ===========
 
 Check if a given mail address exists on a mail server
+
+---
+
+Usually, I search MX servers of a domain and use telnet with HELO, MAIL FROM and RCPT TO for checking if a mail address exists.
+
+I wrote this small python script to do the job.
+
+First, it gets list of mail servers of mail's domain, then it checks if mail address exists.
+
+Usage:
+
+    Usage: mailchecker.py [options] mail_address
+    Options:
+      -h, --help            show this help message and exit
+      -d, --debug           Print debug messages
+      --disable-extra-checking
+                            Disable extra-checking
+
+You need python-dns as dependency:
+
+    # aptitude install python-dns
+
+### Examples ###
+
+    $ python mailchecker.py postmaster@gmail.com
+    INFO    Mail address postmaster@gmail.com exists.
+
+Some servers like Yahoo accept all mails and don't tell you if the mail address really exists:
+
+    $ python mailchecker.py ooz9mahLee3aahag@yahoo.com
+    WARNING This mail server seems to accept all mails addresses
+
+Some servers use greylisting such as no-log.org:
+
+    $ python mailchecker.py a-mail-address@no-log.org
+    INFO    MX server mail.no-log.org uses greylisting... Try again in 1 or 2 minutes
+
+In that case, try again 1 or 2 minutes later:
+
+    $ python mailchecker.py a-mail-address@no-log.org
+    WARNING Mail address a-mail-address@no-log.org doesn't exists.
+
+Debug mode will tells you more what script is actually doing:
+
+    $ python mailchecker.py -d a-mail-address@no-log.org
+    DEBUG   I will check if a-mail-address@no-log.org exists
+    DEBUG   MX hosts for domain no-log.org:
+    DEBUG    * 10 mail.no-log.org
+    DEBUG   Connecting to 10 mail.no-log.org
+    DEBUG   Return code 550: 5.1.1 <a-mail-address@no-log.org>: Recipient address rejected: User unknown in virtual mailbox table
+    WARNING Mail address a-mail-address@no-log.org doesn't exists.
---
+++ mailchecker.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python
+import DNS
+import smtplib
+import sys
+import logging
+import string  # string.ascii_letters
+import random
+from optparse import OptionParser
+import re
+
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#The background is set with 40 plus the number of the color, and the foreground
+#with 30
+
+#These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+
+def formatter_message(message, use_color=True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ) \
+            .replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
+
+class CheckMail(object):
+    def __init__(self, fakedomain='example.com', fakeuser='johndoe', log=False):
+        self.fakedomain = fakedomain
+        self.fakeuser = fakeuser
+        self.smtp = smtplib.SMTP()
+        DNS.DiscoverNameServers()
+        self.logger = log and log or logging.getLogger()
+        self.domain = ''
+        self.mx_hosts = []
+
+    def get_mx_hosts(self, mail):
+        domain = mail.split('@')[1]
+        if domain != self.domain or not self.mx_hosts:
+            self.domain = domain
+            try:
+                self.mx_hosts = DNS.mxlookup(self.domain)
+            except DNS.Base.ServerError:
+                self.logger.error('It seems this domain has no MX entry: %s' % self.domain)
+                sys.exit(1)
+        self.logger.debug('MX hosts for domain %s:' % self.domain)
+        for priority, mail_server in self.mx_hosts:
+            self.logger.debug(" * %s %s" % (priority, mail_server))
+        return self.mx_hosts
+
+    def generate_random_user(self, size=12, chars=string.ascii_letters + string.digits):
+        return ''.join(random.choice(chars) for x in range(size))
+
+    def mail_server_accepts_all_mails(self):
+        self.logger.debug('I will check if mail server accepts all mails')
+        extra_check_result = True
+        for x in xrange(3):
+            random_user = self.generate_random_user()
+            random_mail = '@'.join([random_user, self.domain])
+            self.logger.debug('I will check if %s exists' % random_mail)
+            code, msg = self.smtp.rcpt(random_mail)
+            if code != 250:
+                extra_check_result = False
+                break
+        if extra_check_result:
+            self.logger.warn('This mail server seems to accept all mails addresses')
+            return True
+        return False
+
+    def is_email(self, mail):
+        email_re = re.compile(
+            r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
+            r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"' # quoted-string
+            r')@(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?$', re.IGNORECASE)  # domain
+        return email_re.search(mail)
+
+    def exists(self, mail, extra_checking=False):
+        if not self.is_email(mail):
+            self.logger.warn('%s seems to not be a valid mail address' % mail)
+            sys.exit(1)
+
+        self.logger.debug("I will check if %s exists" % mail)
+        for priority, mail_server in self.get_mx_hosts(mail):
+            try:
+                self.logger.debug('Connecting to %s %s' % (priority, mail_server))
+                self.smtp.connect(mail_server)
+            except smtplib.SMTPConnectError:
+                self.logger.debug('Connection to %s failed' % (priority, mail_server))
+                continue
+
+            code, msg = self.smtp.helo(self.fakedomain)
+            if code != 250:
+                self.logger.error('HELO step failed: %s %s' % (code, msg))
+
+            code, msg = self.smtp.mail('@'.join([self.fakeuser, self.fakedomain]))
+            if code != 250:
+                self.logger.error('MAIL FROM step failed: %s' % msg)
+
+            code, msg = self.smtp.rcpt(mail)
+            if code == 250:
+                if extra_checking:
+                    if self.mail_server_accepts_all_mails():
+                        return False
+                self.logger.info("Mail address %s exists." % mail)
+
+                self.smtp.close()
+                return True
+            elif code == 450:
+                self.logger.info("MX server %s uses greylisting... Try again in 1 or 2 minutes" % mail_server)
+            else:
+                self.logger.debug("Return code %s: %s" % (code, msg))
+                self.logger.warn("Mail address %s doesn't exists." % mail)
+            self.smtp.close()
+            return False
+
+if __name__ == "__main__":
+    parser = OptionParser(usage="usage: %prog [options] mail_address")
+    parser.add_option("-d", "--debug", action="store_true", dest="debug", default=False,
+                      help="Print debug messages")
+    parser.add_option("--disable-extra-checking", action="store_true", dest="disable_extra_checking", default=False,
+                      help="Disable extra-checking")
+
+    # Parse and analyse args
+    (options, args) = parser.parse_args()
+    if options.debug:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    extra_checking = True
+    if options.disable_extra_checking:
+        extra_checking = False
+
+    if len(args) == 0:
+        parser.print_help()
+        sys.exit(0)
+
+    log = logging.getLogger('CheckMail')
+    log.setLevel(level)
+    formatter = ColoredFormatter("%(levelname)-18s %(message)s")
+    handler_stream = logging.StreamHandler()
+    handler_stream.setFormatter(formatter)
+    #handler_stream.setLevel(level)
+    log.addHandler(handler_stream)
+
+    cm = CheckMail(log=log)
+    for mail in args:
+        cm.exists(mail, extra_checking)
